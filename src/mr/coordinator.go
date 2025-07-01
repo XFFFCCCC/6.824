d4367e79036d94934349686b1468191ebca8a205
liufan <liufanfilling@163.com>
2025-07-01 19:38:36 +0800

    refactor:优化为一个task

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -20,12 +20,12 @@ type Task struct {
 type Coordinator struct {
 	// Your definitions here.
 
-	Files       []string //输入文件列表
-	NReduce     int      //Reduce 任务数量
-	MapTasks    []Task
-	ReduceTasks []Task
-	Stage       string //"map","reduce","done"
-	mu          sync.Mutex
+	Files   []string //输入文件列表
+	NReduce int      //Reduce 任务数量
+	Tasks   []Task
+	// ReduceTasks []Task
+	Stage string //"map","reduce","done"
+	mu    sync.Mutex
 }
 
 // 判断目前的状态  ，先硬编码
@@ -54,34 +54,34 @@ func (c *Coordinator) GetTask(request *Request, response *Response) error {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	if c.isMapStage() {
-		for i, task := range c.MapTasks {
+		for i, task := range c.Tasks {
 			// log.Print(i, task.State, "Map")
 			if task.State == 0 {
 				//分配任务
 				// log.Print(i, task.State, "Map")
-				c.MapTasks[i].State = 1
+				c.Tasks[i].State = 1
 				response.FileName = task.Filename
 				response.JobType = TaskTypeMap
 				response.JobNum = i
 				response.ReduceNum = c.NReduce
-				c.MapTasks[i].StartTime = time.Now()
-				response.MapNum = len(c.MapTasks)
+				c.Tasks[i].StartTime = time.Now()
+				response.MapNum = len(c.Files)
 				return nil
 			}
 		}
 
 		//这个地方应该协程睡眠
 	} else if c.isReduceStage() {
-		for i, task := range c.ReduceTasks {
+		for i, task := range c.Tasks {
 			// log.Print(i, task.State, "reduce")
 			if task.State == 0 {
 				//分配任务
-				c.ReduceTasks[i].State = 1
-				c.ReduceTasks[i].StartTime = time.Now()
+				c.Tasks[i].State = 1
+				c.Tasks[i].StartTime = time.Now()
 				response.JobType = TaskTypeReduce
 				response.JobNum = i
 				response.ReduceNum = c.NReduce
-				response.MapNum = len(c.MapTasks)
+				response.MapNum = len(c.Files)
 				return nil
 			}
 		}
@@ -98,11 +98,8 @@ func (c *Coordinator) GetTask(request *Request, response *Response) error {
 func (c *Coordinator) ReportTaskDone(args *ReportTaskArgs, reply *ReportTaskTaskReply) error {
 	c.mu.Lock()
 	defer c.mu.Unlock()
-	if args.JobType == TaskTypeMap {
-		c.MapTasks[args.JobNum].State = 2
-	} else if args.JobType == TaskTypeReduce {
-		c.ReduceTasks[args.JobNum].State = 2
-	}
+
+	c.Tasks[args.JobNum].State = 2
 	reply.Ack = true
 	return nil
 }
@@ -136,9 +133,16 @@ func (c *Coordinator) Done() bool {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	ret := false
-	if c.isMapStage() && c.allTasksDone(c.MapTasks) {
+	if c.isMapStage() && c.allTasksDone(c.Tasks) {
 		c.Stage = "reduce"
-	} else if c.isReduceStage() && c.allTasksDone(c.ReduceTasks) {
+		c.Tasks = make([]Task, c.NReduce)
+		for i := 0; i < c.NReduce; i++ {
+			c.Tasks[i] = Task{
+				TaskId: i,
+				State:  0,
+			}
+		}
+	} else if c.isReduceStage() && c.allTasksDone(c.Tasks) {
 		c.Stage = "done"
 		ret = true
 	}
@@ -151,27 +155,17 @@ func (c *Coordinator) Done() bool {
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	// fmt.Print(files)
 	c := Coordinator{
-		Files:       files,
-		NReduce:     nReduce,
-		Stage:       "map",
-		MapTasks:    make([]Task, len(files)),
-		ReduceTasks: make([]Task, nReduce),
+		Files:   files,
+		NReduce: nReduce,
+		Stage:   "map",
+		Tasks:   make([]Task, len(files)),
 	}
 
 	for i, fname := range files {
-
-		c.MapTasks[i] = Task{
+		c.Tasks[i] = Task{
 			TaskId:   i,
 			Filename: fname,
-
-			State: 0, //idle
-		}
-	}
-
-	for i := 0; i < nReduce; i++ {
-		c.ReduceTasks[i] = Task{
-			TaskId: i,
-			State:  0,
+			State:    0, //idle
 		}
 	}
 
@@ -196,22 +190,14 @@ func MakeCoordinator(files []string, nReduce int) *Coordinator {
 			checkTimeout := func(t Task) bool {
 				return t.State == 1 && time.Since(t.StartTime) > 10*time.Second
 			}
-			if c.isMapStage() {
-				for i := range c.MapTasks {
-					if checkTimeout(c.MapTasks[i]) {
-						// log.Printf("Map task %d timed out. Reassigning...\n", i)
-						c.MapTasks[i].State = 0
-					}
-				}
 
-			} else if c.isReduceStage() {
-				for i, t := range c.ReduceTasks {
-					if checkTimeout(t) {
-						// log.Printf("Reduce task %d timed out. Reassigning...\n", i)
-						c.ReduceTasks[i].State = 0
-					}
+			for i := range c.Tasks {
+				if checkTimeout(c.Tasks[i]) {
+					// log.Printf("Map task %d timed out. Reassigning...\n", i)
+					c.Tasks[i].State = 0
 				}
 			}
+
 			//c.Done()
 			c.mu.Unlock()
 		}
